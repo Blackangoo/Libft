9c316b25b80d12479ba46b664c5392b9cff7697d
blackangoo <blackangoo.gc@gmail.com>
2023-08-27 22:40:47 +0200

    normined libft.h

--- libft.h
+++ libft.h
@@ -1,22 +1,22 @@
 #ifndef LIBFT_H
-    #define LIBFT_H
+# define LIBFT_H
 
-    #include <string.h>
+# include <string.h>
 
-    typedef struct s_list
-    {
-        struct s_list *next;
-	    void *data;
-    } t_list;
+typedef struct s_list
+{
+	struct s_list	*next;
+	void			*data;
+}	t_list;
 
-    int ft_isalpha(int c);
-    int ft_isdigit(int c);
-    int ft_isalnum(int c);
-    int ft_isascii(int c);
-    int ft_isprint(int c);
-    int ft_strlen(char *str);
-    void *ft_memset(void *s, int c, size_t n);
-    void ft_bzero(void *s, size_t n);
-    void *ft_memcpy(void *dest, const void *src, size_t n);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_strlen(char *str);
+void	*ft_memset(void *s, int c, size_t n);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
 
 #endif
