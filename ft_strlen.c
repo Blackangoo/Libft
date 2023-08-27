47752ee5a7f7f127ff3ea9224586ec9d608b157e
blackangoo <blackangoo.gc@gmail.com>
2023-08-27 22:33:10 +0200

    normined ft_strlen

--- ft_strlen.c
+++ ft_strlen.c
@@ -1,11 +1,11 @@
 #include "libft.h"
 
-int ft_strlen(char *str)
+int	ft_strlen(char *str)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (*(str + i) != '\0')
-        i++;
-    return (i);
+	i = 0;
+	while (*(str + i) != '\0')
+		i++;
+	return (i);
 }
