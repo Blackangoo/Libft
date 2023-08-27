294a7aa83c0b183a0e793da51786b04d15352513
blackangoo <blackangoo.gc@gmail.com>
2023-08-27 22:31:45 +0200

    normined ft_memset

--- ft_memset.c
+++ ft_memset.c
@@ -1,14 +1,14 @@
 #include "libft.h"
 
-void *ft_memset(void *s, int c, size_t n)
+void	*ft_memset(void *s, int c, size_t n)
 {
-    unsigned char	*s1;
+	unsigned char	*s1;
 
 	s1 = (unsigned char *) s;
 	while (n)
 	{
 		*(s1++) = (unsigned char) c;
-        n--;
+		n--;
 	}
 	return (s);
 }
