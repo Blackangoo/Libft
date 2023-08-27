894062e3b2ef6ace483f1a36779d71ae89ddef9f
blackangoo <blackangoo.gc@gmail.com>
2023-08-27 22:45:36 +0200

    added the case when NULL as both params with size

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -7,6 +7,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 
 	destination = (unsigned char *)dest;
 	source = (unsigned char *)src;
+	if (!dest && !src)
+		return (NULL);
 	while (n)
 	{
 		*(destination++) = *(source++);
