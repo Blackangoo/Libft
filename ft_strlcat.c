e260922168662cd1b48ca8146ce205590baecf47
blackangoo <blackangoo.gc@gmail.com>
2023-08-27 23:56:20 +0200

    normined ft_strlact

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -3,19 +3,19 @@
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	dest_len;
-    size_t	src_len;
-    size_t	i;
+	size_t	src_len;
+	size_t	i;
 
-    dest_len = ft_strlen(dst);
-    src_len = ft_strlen(src);
-    i = 0;
-    if (dstsize - 1 <= dest_len)
-        return(src_len + dstsize);
-    while (dest_len + i < dstsize - 1)
-    {
-        dst[dest_len + i] = src[i];
-        i++;
-    }
-    dst[dest_len + i] = '\0';
-    return(dest_len + src_len);
+	dest_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
+	i = 0;
+	if (dstsize - 1 <= dest_len)
+		return (src_len + dstsize);
+	while (dest_len + i < dstsize - 1)
+	{
+		dst[dest_len + i] = src[i];
+		i++;
+	}
+	dst[dest_len + i] = '\0';
+	return (dest_len + src_len);
 }
