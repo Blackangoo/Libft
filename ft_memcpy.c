a13ee9dfc5b2d4848c0eec7267d6cf1f3a94f38d
blackangoo <blackangoo.gc@gmail.com>
2023-08-27 22:24:28 +0200

    normined ft_memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,18 +1,16 @@
 #include "libft.h"
 
-void *ft_memcpy(void *dest, const void *src, size_t n)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-    unsigned char *destination;
-    unsigned char *source;
+	unsigned char	*destination;
+	unsigned char	*source;
 
-    destination = (unsigned char *)dest;
-    source = (unsigned char *)src;
-
-    while (n)
-    {
-        *(destination++) = *(source++);
-        n--;
-    }
-    return(dest);
-
+	destination = (unsigned char *)dest;
+	source = (unsigned char *)src;
+	while (n)
+	{
+		*(destination++) = *(source++);
+		n--;
+	}
+	return (dest);
 }
