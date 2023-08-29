35668fe75929f6ceba66a6e9fe135bfad844eecf
blackangoo <blackangoo.gc@gmail.com>
2023-08-29 11:10:36 +0200

    corrected ft_strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -3,15 +3,15 @@
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	dest_len;
-	size_t	src_len;
 	size_t	i;
-
-	dest_len = ft_strlen(dst);
+	size_t	src_len;
+	
 	src_len = ft_strlen(src);
+	dest_len = ft_strlen(dst);
 	i = 0;
-	if (dstsize - 1 <= dest_len)
+	if (dstsize <= dest_len)
 		return (src_len + dstsize);
-	while (dest_len + i < dstsize - 1)
+	while (src[i] && dest_len + i < dstsize - 1)
 	{
 		dst[dest_len + i] = src[i];
 		i++;
